6c09cbe44f32ab56b307c65678881c59029ac488
Yichun Jin <jinyichun17@gmail.com>
2021-06-07 16:39:49 -0700

    Added style seperator

--- client/src/relatedItems/relatedItemsAndComparison.jsx
+++ client/src/relatedItems/relatedItemsAndComparison.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 const RelatedItemsAndComparison = ( {productId, setProductId}) => {
   console.log(`ProductId in relatedItems is ${productId}`);
-  setInterval(() => {
-    setProductId(previousState => previousState + 1);
-  }, 2000);
 
   return (
     <div>
