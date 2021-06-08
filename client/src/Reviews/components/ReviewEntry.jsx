136010c9cacdf8f476953cab703ec6779ba355dc
Simon <simonglu2@gmail.com>
2021-06-08 13:26:19 -0700

    Refactored to use dynamic product id

--- client/src/Reviews/components/ReviewEntry.jsx
+++ client/src/Reviews/components/ReviewEntry.jsx
@@ -1,50 +1,36 @@
 import React from 'react';
 import axios from 'axios';
-
 class ReviewEntry extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       entry: []
     };
   }
 
-  componentDidMount() {
-    axios.post('/getReview', { productID: '13023' })
-      .then((res) => {
-        res.data.forEach((element) => {
-          this.setState({
-            entry: this.state.entry.concat(element)
-          });
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        return;
-      });
-  }
-
   render() {
     return (
       <div>
-
-        {this.state.entry.map((element) => (
-          <div className='individualReview'>
-            <p className='rating'>Rating: {element.rating}</p>
-            <p className='reviewerName'>{element.reviewer_name}</p>
-            <p className='reviewData'>{Date(element.date).substring(0,15)}</p>
-            <p className='summary'>{element.summary}</p>
-            <p className='body'>{element.body}</p>
-            <p className='helpfulness'>Helpful? {element.helpfulness} | Report</p>
-            {element.photos.length > 0 ?
-              <div className='reviewImages'>
-                <img src={element.photos[0].url} height="100" width="100"></img>
-              </div> :
-              <img src=''></img>
-            }
-          </div>
-        ))}
+        <div>{this.props.currentProduct !== undefined && Object.keys(this.props.currentProduct).length !== 0 ?
+          <div>{this.props.currentProduct.reviews.results.map((element) =>
+            <div className='individualReview' key={element.review_id}>
+              <p className='rating'>Rating: {element.rating}</p>
+              <p className='reviewerName'>{element.reviewer_name}</p>
+              <p className='reviewData'>{Date(element.date).substring(4, 15)}</p>
+              <p className='summary'>{element.summary}</p>
+              <p className='body'>{element.body}</p>
+              <p className='helpfulness'>Helpful? {element.helpfulness} | Report</p>
+              {element.photos.length > 0 ?
+                <div className='reviewImages'>
+                  <img src={element.photos[0].url} height="100" width="100"></img>
+                </div> :
+                <img src=''></img>
+              }
+            </div>)}
+          </div> :
+          console.log()
+        }</div>
       </div>
     );
   }
