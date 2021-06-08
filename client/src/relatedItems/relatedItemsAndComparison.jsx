34e1692730d0198dd94f7ecf48da238f0a387e95
Joey Rivera <josephrivera5000@gmail.com>
2021-06-07 17:07:19 -0700

    added useEffect to fetch relatedItem ids from relatedItems endpoint

--- client/src/relatedItems/relatedItemsAndComparison.jsx
+++ client/src/relatedItems/relatedItemsAndComparison.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const RelatedItemsAndComparison = ( {productId, setProductId}) => {
+  const [relatedItemIds, setRelatedItemIds] = useState([]);
+  // https://app-hrsei-api.herokuapp.com/api/fec2/hr-sjo/products/${productId}
 
+  useEffect(async () => {
+    try {
+      let response = await fetch(`http://localhost:1128/relatedItems`);
+      if (!response.ok) {
+        throw 'Error while fetching related items';
+      }
+      let data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [productId]);
 
   return (
     <div>
-      <p>{productId}</p>
+      <p>productId: {productId}</p>
+      <p>Related Item Ids: {relatedItemIds}</p>
     </div>
   );
 };
