0c37a2725fc199277c3c83a4e06d588528218b73
Yichun Jin <jinyichun17@gmail.com>
2021-06-07 19:54:25 -0700

    Added size selector

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/getReview', (req, res) => {
     headers: { Authorization: APIToken.TOKEN}
   })
     .then((response) => {
-      console.log(response.data.results);
+
       res.send(response.data.results);
     })
     .catch((err) => {
