3b6e80ac8af934655bbdc65b0a6bd67ac5326396
Joey Rivera <josephrivera5000@gmail.com>
2021-06-08 16:40:27 -0700

    Added a placeholder for the action button

--- client/src/relatedItems/relatedCard.jsx
+++ client/src/relatedItems/relatedCard.jsx
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from 'react';
 const RelatedCard = ({relatedItem}) => {
   const [action, setAction] = useState();
   const imageURL = relatedItem.styles.results[0].photos[0].thumbnail_url;
-  console.log(imageURL);
 
   return (
     <table className='related-items'>
       <tbody>
         <tr>
+          <button onClick={() => console.log('PLACEHOLDER')}>Action</button>
           <td><img src={imageURL !== null ? imageURL : 'https://bit.ly/3507jj5'} height="100" width="100"></img></td>
           <td>{relatedItem.overview.category}</td>
           <td>{relatedItem.overview.name}</td>
