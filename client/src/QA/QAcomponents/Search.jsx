e61958e79dbcc90dfe360feda5314d18116c539c
Anan Wolf <ananwolf1@gmail.com>
2021-06-09 14:20:49 -0700

    input Material-ui styling to Q&A

--- client/src/QA/QAcomponents/Search.jsx
+++ client/src/QA/QAcomponents/Search.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
-const Search = (props) => (
-  <div>
-    <input className='qa-searchbar' type='text' placeholder='Search for Answers...'></input>
-  </div>
-);
+const Search = ({ handleSearch }) => {
+
+  const handleFocus = (event) => {
+    event.preventDefault();
+    const { target } = event;
+    target.setSelectionRange(0, target.value.length);
+  };
+
+  return (
+    <div>
+      <form className='qa-search'>
+        <input style={{
+          fontSize: 14,
+          fontWeight: 600,
+          marginLeft: 10,
+          marginBottom: 10,
+        }}
+        className='qa-search-bar'
+        placeholder='Search for Answers...'
+        onChange={event => handleSearch(event.target.value)}
+        onFocus={handleFocus}></input>
+      </form>
+      {/* <input className='qa-searchbar' type='text' placeholder='Search for Answers...'></input> */}
+    </div>
+  );
+};
 
 export default Search;
 
--- client/src/QA/QAwidget.jsx
+++ client/src/QA/QAwidget.jsx
@@ -1,35 +1,105 @@
+// Main functinalities imports
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import sample from './sampleData.js';
-
+// JSX imports
 import Search from './QAcomponents/Search.jsx';
 import QuestionList from './QAcomponents/QuestionList.jsx';
 import Helpful from './QAcomponents/Helpful.jsx';
 import AddQuestion from './QAcomponents/AddQuestion.jsx';
+
+// Material-UI imports
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: '80vw'
+  },
+  questionsStyle: {
+    maxHeight: '50vh',
+    overflow: 'scroll'
+  }
+}));
 
 const QAwidget = ( { currentProduct } ) => {
 
   // const [questions, setQuestions] = useState(sample.questions);
   const [searchInput, setSearchInput] = useState('');
   const [product, setProduct] = useState('');
+  const classes = useStyles();
+
+  let handleSearch = (searchInput) => {
+    searchInput.length > 2
+      ? setSearchInput(searchInput)
+      : setSearchInput('');
+  };
+
+  // Bindings
+  handleSearch = handleSearch.bind(this);
 
   return (
-    <div>
-      <div>
+    <Box elevation={0} className={classes.root}>
 
-        <div>Question Answers</div>
-        <Search />
-        {currentProduct !== undefined && Object.keys(currentProduct).length !== 0 ?
-          <QuestionList
-            searchInput={searchInput}
-            questions={currentProduct.questionsAnswers.results} />
-          : console.log('loading Q&A')}
-        {/* <Helpful report={'Add Answer'} /> */}
-        <AddQuestion productID={currentProduct.product_id}/>
+      <Grid container direction='column' spacing={1}>
+        <Grid item>
+          <Typography variant='h5'
+            style={{
+              paddingBottom: 0,
+              paddingTop: 4,
+              matgin: '10px 0px 0px 10px'
+            }}>Question and Answers
+          </Typography>
+        </Grid>
 
-      </div>
-    </div>
+        <Grid item>
+          <Search handleSearch={handleSearch} />
+        </Grid>
+
+        <Grid item className={classes.questionsStyle}>
+          <Grid container direction='column' spacing={3} style={{
+            maxWidth: '97%'
+          }}>{currentProduct !== undefined && Object.keys(currentProduct).length !== 0 ?
+              <QuestionList
+                searchInput={searchInput}
+                questions={currentProduct.questionsAnswers.results} />
+              : console.log('loading Q&A')}
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container justify='center' alignItems='center' spacing={1}>
+        ADD FUNCTIONALITIES TO EXPAND/COLLAPSE QUESTIONS HERE
+
+        <Grid item>
+          <AddQuestion
+            productID={currentProduct.product_id} />
+        </Grid>
+      </Grid>
+
+    </Box>
+
+
+  // <div>
+  //   <div>
+
+  //     <div>Question Answers</div>
+  //     <Search
+  //       handleSearch={handleSearch} />
+
+  //     {currentProduct !== undefined && Object.keys(currentProduct).length !== 0 ?
+  //       <QuestionList
+  //         searchInput={searchInput}
+  //         questions={currentProduct.questionsAnswers.results} />
+  //       : console.log('loading Q&A')}
+
+  //     <AddQuestion productID={currentProduct.product_id}/>
+
+  //   </div>
+  // </div>
 
   );
 };
