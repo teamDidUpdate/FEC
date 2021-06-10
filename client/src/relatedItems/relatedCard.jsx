1db794b940a86d75b58ac1fdfbe7e015c6c3289f
Joey Rivera <josephrivera5000@gmail.com>
2021-06-09 20:09:46 -0700

    Partially added scrolling behavior

--- client/src/relatedItems/relatedCard.jsx
+++ client/src/relatedItems/relatedCard.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const RelatedCard = ({relatedItem}) => {
+const RelatedCard = ({ product }) => {
   const [action, setAction] = useState();
-  const imageURL = relatedItem.styles.results[0].photos[0].thumbnail_url;
+  const imageURL = product.styles.results[0].photos[0].thumbnail_url;
 
   return (
     <div className='related-items grid-container'>
@@ -12,9 +12,9 @@ const RelatedCard = ({relatedItem}) => {
       <div className='grid-item'>
         <img className='related-image' src={imageURL !== null ? imageURL : 'https://bit.ly/2Tg8g4s'}></img>
       </div>
-      <div className='grid-item'>{relatedItem.overview.category}</div>
-      <div className='grid-item'>{relatedItem.overview.name}</div>
-      <div className='grid-item'>{relatedItem.overview.default_price}</div>
+      <div className='grid-item'>{product.overview.category}</div>
+      <div className='grid-item'>{product.overview.name}</div>
+      <div className='grid-item'>{product.overview.default_price}</div>
       <div className='grid-item'>RATING PLACEHOLDER</div>
     </div>
   );
--- client/src/relatedItems/relatedItemsAndComparison.jsx
+++ client/src/relatedItems/relatedItemsAndComparison.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import RelatedCard from './RelatedCard.jsx';
 import axios from 'axios';
 
-const RelatedItemsAndComparison = ({productId, setProductId, getProductById}) => {
+const RelatedItemsAndComparison = ({productId, setProductId}) => {
   const [relatedProducts, setRelatedProducts] = useState([]);
+  const [productLocation, setProductLocation] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [scrollable, setScrollable] = useState({left: false, right: true});
 
+  // get related products when the productId changes
   useEffect(() => {
     axios.get('/relatedIds', { params: { productId: productId } })
       .then((response) => {
@@ -17,6 +21,7 @@ const RelatedItemsAndComparison = ({productId, setProductId, getProductById}) =>
       });
   }, [productId]);
 
+  // gets related products for each id
   const getRelatedProducts = async (ids) => {
     try {
       let items = await ids.map(id => {
@@ -29,6 +34,7 @@ const RelatedItemsAndComparison = ({productId, setProductId, getProductById}) =>
     }
   };
 
+  // fetches a related product from the server
   const getRelatedProductById = async (id) => {
     let relatedProduct = {};
     await axios.get('/relatedProduct', { params: { productId: id } })
@@ -42,20 +48,82 @@ const RelatedItemsAndComparison = ({productId, setProductId, getProductById}) =>
     return relatedProduct;
   };
 
+  // check to see if related products should be scrollable
+  useEffect(() => {
+    const screenBuffer = 200;
+    let productWidth = document.getElementById('related-carousel').clientWidth;
+    if (productWidth + screenBuffer < screenWidth) {
+      setScrollable({ ...scrollable, right: false });
+    }
+    if (productWidth + screenBuffer < screenWidth) {
+      setScrollable({ ...scrollable, right: true });
+    }
+  }), [screenWidth];
+
+  // determine if the left scroll button should display
+  useEffect(() => {
+    if (productLocation <= 0) {
+      setScrollable({...scrollable, left: false });
+    } else {
+      setScrollable({...scrollable, left: true });
+    }
+  }), [productLocation];
+
+  // carousel functionality
+  const moveCarousel = (direction, event) => {
+    // think about how to refactor significan selection
+    let area = event.target.parentNode.parentNode.children[1];
+    let relatedWidth = event.target.parentNode.parentNode.children[1].children[1].clientWidth;
+
+    if (direction === 'right') {
+      area.scrollLeft += relatedWidth;
+      let loc = area.scrollLeft + relatedWidth;
+      setProductLocation(loc);
+    } else {
+      area.scrollLeft -= relatedWidth;
+      let loc = area.scrollLeft - relatedWidth;
+      setProductLocation(loc);
+    }
+  };
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
 
   return (
-    <div className='related-items-and-comparison grid-outer-container'>
-      {relatedProducts.length !== 0 ? relatedProducts.map(relatedItem => {
-        return (
-          <div
-            className='related-card grid-container'
-            key={relatedItem.overview.id}
-            onClick={() => setProductId(relatedItem.overview.id)}
-          >
-            <RelatedCard relatedItem={relatedItem}/>
-          </div>
-        );
-      }) : null}
+    <div className='related-items-and-comparison grid-outer-container' id='related-carousel'>
+      <div>
+        <h2>Related Products</h2>
+      </div>
+      <div id='relatedWrapper'>
+        <button
+          show={scrollable.left}
+          position={'left'}
+          onClick={(e) => moveCarousel('left', e)}
+        >
+          LEFT
+        </button>
+        <div id='relatedCardContainer'>
+          {relatedProducts.length !== 0 ? relatedProducts.map(product => {
+            return (
+              <div
+                className='related-card grid-container'
+                key={product.overview.id}
+                onClick={() => setProductId(product.overview.id)}
+              >
+                <RelatedCard product={product}/>
+              </div>
+            );
+          }) : null}
+        </div>
+        <button
+          show={scrollable.right}
+          position={'right'}
+          onClick={(e) => moveCarousel('right', e)}
+        >
+          RIGHT
+        </button>
+      </div>
     </div>
   );
 };
