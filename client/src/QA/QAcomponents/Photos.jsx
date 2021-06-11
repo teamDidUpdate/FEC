a0fedb8efeb69f93513f57df58c2e15101e1057a
Anan Wolf <ananwolf1@gmail.com>
2021-06-11 15:11:10 -0700

    implement testing and changes to Q&A

--- client/src/QA/QAcomponents/Photos.jsx
+++ client/src/QA/QAcomponents/Photos.jsx
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
-const Photos = ({ photo }) => (
-  <img src={photo} className='photo' width={75} height={75}></img>
-);
+const Photos = ({ photo }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const imgClick = () => setClicked(!clicked);
+
+  const smallPic = (
+    <div>
+      <img
+        src={photo}
+        onClick={() => imgClick()}
+        style={{
+          maxWidth: '100%',
+        }}
+      ></img>
+    </div>
+  );
+
+  return (
+    <div style={{
+      marginRight: '1.5%',
+      display: 'inline-flex',
+      flexDirection: 'row'
+    }}>
+      <img
+        className='photo'
+        src={photo}
+        width={75}
+        height={75}></img>
+    </div>
+  );
+};
 
 export default Photos;
+
+{/* <img src={photo} className='photo' width={75} height={75}></img> */}
