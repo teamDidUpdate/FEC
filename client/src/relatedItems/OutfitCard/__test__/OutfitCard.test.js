551d4d750eb459a7ccb7f5bd57d5d586151b55a5
Joey Rivera <josephrivera5000@gmail.com>
2021-06-15 11:33:19 -0700

    Added one passing test per related item component

--- client/src/relatedItems/OutfitCard/__test__/OutfitCard.test.js
+++ client/src/relatedItems/OutfitCard/__test__/OutfitCard.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import OutfitCard from '../OutfitCard.jsx';
+import productWithStyle from '../../../../../data/sampleRelatedProducts.js';
 
 it ('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<OutfitCard/>, div);
+  ReactDOM.render(<OutfitCard outfit={productWithStyle}/>, div);
 });
