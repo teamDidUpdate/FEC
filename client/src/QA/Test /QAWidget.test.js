c7538f71fd239d84ca9b2c8a9617ff3777f16535
Anan Wolf <ananwolf1@gmail.com>
2021-06-19 09:03:32 -0700

    [ulling

--- client/src/QA/Test /QAWidget.test.js
+++ client/src/QA/Test /QAWidget.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import QAWidget from '../QAWidget.jsx';
+import { render, screen, cleanup } from '@testing-library/react';
 
-it ('render Q&A without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<QAWidget />, div);
+test ('render Q&A without crashing', () => {
+  render(<QAWidget/>);
+  const qawidget = screen.getByTestId();
+  expect(qawidget).toBeInTheDocument();
+  expect(qawidget).toHaveTextContent('Questions & Answers');
 });
--- client/src/QA/Test /QAcomponents.test.js
+++ client/src/QA/Test /QAcomponents.test.js
@@ -1,61 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Search from '../QAcomponents/Search.jsx';
-import QuestionList from '../QAcomponents/QuestionList.jsx';
-import AddQuestion from '../QAcomponents/AddQuestion.jsx';
-import AnswerHelpful from '../QAcomponents/AnswerHelpful.jsx';
-import AnswerPhotos from '../QAcomponents/AnswerPhotos.jsx';
-import Answers from '../QAcomponents/Answers.jsx';
-import MoreAnswers from '../QAcomponents/MoreAnswers.jsx';
-import Question from '../QAcomponents/Question.jsx';
-import QuestionHelpful from '../QAcomponents/QuestionHelpful.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Search from '../QAcomponents/Search.jsx';
+// import QuestionList from '../QAcomponents/QuestionList.jsx';
+// import AddQuestion from '../QAcomponents/AddQuestion.jsx';
+// import AnswerHelpful from '../QAcomponents/AnswerHelpful.jsx';
+// import AnswerPhotos from '../QAcomponents/AnswerPhotos.jsx';
+// import Answers from '../QAcomponents/Answers.jsx';
+// import MoreAnswers from '../QAcomponents/MoreAnswers.jsx';
+// import Question from '../QAcomponents/Question.jsx';
+// import QuestionHelpful from '../QAcomponents/QuestionHelpful.jsx';
 
-it ('render Q&A Search without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Search />, div);
-});
+// it ('render Q&A Search without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Search />, div);
+// });
 
-it ('render Search without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AnswerHelpful />, div);
-});
+// it ('render Search without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<AnswerHelpful />, div);
+// });
 
-it ('render Question List without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<QuestionList />, div);
-});
+// it ('render Question List without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<QuestionList />, div);
+// });
 
-it ('render Add Question Button Modal crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AddQuestion />, div);
-});
+// it ('render Add Question Button Modal crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<AddQuestion />, div);
+// });
 
-it ('render Answer Helpfulness without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AnswerHelpful />, div);
-});
+// it ('render Answer Helpfulness without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<AnswerHelpful />, div);
+// });
 
-it ('render Answer Photos without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AnswerPhotos />, div);
-});
+// it ('render Answer Photos without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<AnswerPhotos />, div);
+// });
 
-it ('render Answer List without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Answers />, div);
-});
+// it ('render Answer List without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Answers />, div);
+// });
 
-it ('render More Answer button without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MoreAnswers />, div);
-});
+// it ('render More Answer button without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<MoreAnswers />, div);
+// });
 
-it ('render Individual Questions without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Question />, div);
-});
+// it ('render Individual Questions without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Question />, div);
+// });
 
-it ('render Question Helpfulness section without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<QuestionHelpful />, div);
-});
+// it ('render Question Helpfulness section without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<QuestionHelpful />, div);
+// });
