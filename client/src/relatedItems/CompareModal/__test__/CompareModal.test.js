7b544dba0c2c14e2f01c67369c59b4f26cbc3dd0
Joey Rivera <josephrivera5000@gmail.com>
2021-06-19 15:56:00 -0700

    Implemented snapshot tests

--- client/src/relatedItems/CompareModal/__test__/CompareModal.test.js
+++ client/src/relatedItems/CompareModal/__test__/CompareModal.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CompareModal from '../CompareModal.jsx';
 
-it ('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CompareModal/>, div);
 });
--- client/src/relatedItems/RelatedCard/__test__/RelatedCard.test.js
+++ client/src/relatedItems/RelatedCard/__test__/RelatedCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
-import {renderer, act } from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
 import RelatedCard from '../RelatedCard.jsx';
 import productWithStyle from '../../../../../data/sampleRelatedProducts.js';
@@ -9,8 +9,13 @@ afterEach(() => {
   cleanup();
 });
 
-it ('renders without crashing', () => {
+test('renders a related card without crashing', () => {
   render(<RelatedCard product={productWithStyle[0]} />);
   const relatedElement = screen.getByTestId('related-13023');
   expect(relatedElement).toBeInTheDocument();
+});
+
+test('related card matches snapshot', () => {
+  const relatedTree = renderer.create(<RelatedCard product={productWithStyle[0]} />).toJSON();
+  expect(relatedTree).toMatchSnapshot();
 });
